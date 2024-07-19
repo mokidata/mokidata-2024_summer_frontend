1c5b1dd3655a93a216cdceb2831ecbec333722df
Apoliasm <syjsir@gmail.com>
2024-07-19 15:36:37 +0900

    FEAT : Add Login page

---
+++ frontend/src/component/common/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Button (props){
+    return (
+        <button className="button" id={props.color}>
+            {props.txt}
+        </button>
+    )
+
+}
+
+export default Button;  
---
+++ frontend/src/pages/LoginPages/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Button from "../../component/common/Button";
+function Login(){
+    const [passed,setPassed] = useState(true);
+    return (
+        <div className="login-page">
+            
+            <div> 
+                
+            </div>
+            <div className="logo-div">
+                <svg className="logo-img"></svg>
+            </div>
+            <div className="input-div">
+                <div className="login-desc" id='id'>
+                    <p>아이디</p>
+
+                </div>
+                <div className="login-input" id='id'>
+                    <input className="login-input-img" id="id"/> 
+
+                </div>
+                <div className="login-desc" id='pswd'>
+                    <p>비밀번호</p>
+                </div>
+                <div className="login-input">
+                    <input className="login-input-img" id="pswd"/>
+                </div>
+                
+            </div>
+            <div className="button-div">
+                <Button color="black" txt="로그인" ></Button>
+            </div>
+            <div className="login-msg">
+                {passed === true ? "" : "아이디 또는 비밀번호를 잘못 입력했습니다."}
+                
+            </div>
+            
+
+        </div>
+
+    );
+}
+
+export default Login;
