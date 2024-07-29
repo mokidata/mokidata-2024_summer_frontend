b9855b1d49e821574290cfee6ebbf0a669cf1617
Apoliasm <syjsir@gmail.com>
2024-07-29 17:32:43 +0900

    CHORE : Fix css, remove log code

--- frontend/src/component/common/Header.jsx
+++ frontend/src/component/common/Header.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState ,useMemo} from "react";
 import usePageInfo from "../../hooks/usePageInfo";
 
 function Header(props){
-    console.log("header");
     const pageinfo = usePageInfo(props.page);
     
     return (
