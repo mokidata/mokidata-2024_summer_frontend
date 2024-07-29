f23b58089b8d4e0d97276f17e8a9644755841f26
Apoliasm <syjsir@gmail.com>
2024-07-29 17:37:28 +0900

    CHORE : Fix usePageInfo

--- frontend/src/hooks/usePageInfo.jsx
+++ frontend/src/hooks/usePageInfo.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 
 const usePageInfo = (input) => {
-    console.log(input)
     const [pageInfo, setPageInfo] = useState("default");
     
     useEffect(() => {
-        switch(input){
-            case 'daily':
-                setPageInfo("daily");
-                break;
-            case 'weekly':
-                setPageInfo("weekly");
-                break;
-            case 'monthly':
-                setPageInfo("monthly");
-                break;
-            case 'login':
-                setPageInfo("login");
-                break;
+        if (input === 'login'){
+            setPageInfo("login");
         }
-
+        else{
+            switch(input){
+                case 'daily':
+                    setPageInfo("daily");
+                    
+                    break;
+                case 'weekly':
+                    setPageInfo("weekly");
+                    break;
+                case 'monthly':
+                    setPageInfo("monthly");
+                    break;
+                case 'login':
+                    
+                    break;
+            }
+        }
     }, [input]);
    
 
