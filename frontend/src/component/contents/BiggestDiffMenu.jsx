78589539322e1c1460eb7c52659ef33e2c8599a9
Apoliasm <syjsir@gmail.com>
2024-07-30 17:28:06 +0900

    FEAT : Fix BiggestDiffmenu code for connecting API

--- frontend/src/component/contents/BiggestDiffMenu.jsx
+++ frontend/src/component/contents/BiggestDiffMenu.jsx
@@ -1,13 +1,94 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Button from "../common/Button";
 import Triangle from "../common/Triangle";
+import { BASE_URL } from "../Url";
 
 function BiggestDiffMenu(props){
-    // const []
-    // useEffect(()=>{
+    console.log(props.todayValue)
+    console.log(props.lastDetailValue)
 
+    const [salesDetailValue,setSalesDetailValue] = useState({})
+    const [lastDetailValue,setLastDetailValue] = useState({})
+    const [diffArray , setDiffArray] = useState([])
+    const [bestMenu,setBestMenu] = useState({"name":"","percentage" : 0,"diff" : 0,"img":""})
+    const [worstMenu,setWorstMenu] = useState({"name":"","percentage" : 0,"diff" : 0,"img":""})
+    let result = {};
+    useEffect(()=>{
+        
+        if(props.todayValue[props.page]['saleGetResList'] !== undefined){
+            result ={}
+            props.todayValue[props.page]['saleGetResList'].forEach(item => {
+                const { name, count, price } = item;
+                result[name] = { count, price };
+            });
+            setSalesDetailValue(result);
+        }
+        if(props.lastDetailValue[props.page]['saleGetResList'] !== undefined){
+            result ={}
+            props.lastDetailValue[props.page]['saleGetResList'].forEach(item => {
+                const { name, count, price } = item;
+                result[name] = { count, price };
+            });
+            setLastDetailValue(result);
+        }
+    },[props])
+    useEffect(()=>{
+        if(salesDetailValue.length !== 0 && lastDetailValue.length !== 0){
+            const diff = Object.keys(salesDetailValue).map((name)=>{
+                const obj = {}
+                obj['name'] = name
+                const todayProfit = salesDetailValue[name].price * salesDetailValue[name].count
+                const lastProfit = lastDetailValue[name].price * lastDetailValue[name].count
+                obj['diff'] =  todayProfit - lastProfit
+                obj['percentage'] = Math.round((todayProfit - lastProfit) / lastProfit * 100 * 100  )/100 //소숫점 둘째자리로 반올림
+                return obj;
+            })
+            setDiffArray(diff)
+        }
+        
+    },[salesDetailValue,lastDetailValue])
 
-    // },[props.value])
+    useEffect(()=>{
+        // 가자 상승률 높은 객체 찾기
+        if(diffArray.length !== 0){
+            const maxPercentageObject = diffArray.reduce((max, item) => {
+                return item.percentage > max.percentage ? item : max;
+            });
+            //가장 상승률 높은 메뉴의 이미지 찾기
+            if(Object.keys(props.menuObject).length !== 0){ 
+                maxPercentageObject["img"] = props.menuObject[maxPercentageObject.name]['img']
+            }
+            
+
+            // 가장 상승률 낮은 객체 찾기
+            const minPercentageObject = diffArray.reduce((min, item) => {
+                return item.percentage < min.percentage ? item : min;
+            });
+            //가장 상승률 높은 메뉴의 이미지 찾기
+            if(Object.keys(props.menuObject).length !== 0){ 
+                minPercentageObject["img"] = props.menuObject[minPercentageObject.name]['img']
+            }
+
+            setBestMenu(maxPercentageObject)
+            setWorstMenu(minPercentageObject)
+        }
+        
+    },[diffArray])
+
+    useEffect(()=>{
+        
+
+    }, [ bestMenu ])
+
+    
+    /*
+    diffArray [{
+        "name":딸기우유,
+        "percentage" : 12.3
+        "diff" : 12345,
+        "img":""
+    } , ...]
+    */ 
 
     return(
         <div className="report-component" id="biggest-diff-menu" >
@@ -17,17 +98,17 @@ function BiggestDiffMenu(props){
             <div className="diff">
                 <div className="diff-desc" style={{textAlign:"left"}}>
                     <div className="diff-desc__title">
-                        딸기 스무디
+                        {bestMenu.name}
                     </div>
                     <div className="diff-desc__percent" >
-                        13.5 %
+                        {bestMenu.percentage} %
                     </div>
-                    <Triangle diff={123456} unit="원" > 
+                    <Triangle diff={bestMenu.diff} unit="원" > 
 
                     </Triangle>
                     
                 </div>
-                <div className="diff-img">
+                <div className="diff-img" style={{backgroundImage:`url(${BASE_URL}${bestMenu.img})`}}>
 
                 </div>
             </div>
@@ -43,16 +124,16 @@ function BiggestDiffMenu(props){
 
             </div>
             <div className="diff">
-                <div className="diff-img">
+                <div className="diff-img" style={{backgroundImage:`url(${BASE_URL}${worstMenu.img})`}} >
                 </div>
                 <div className="diff-desc" style={{textAlign:"right"}}>
                     <div className="diff-desc__title">
-                        크렌베리 베이글
+                        {worstMenu.name}
                     </div>
                     <div className="diff-desc__percent">
-                        -20.2 %
+                        {worstMenu.percentage} %
                     </div>
-                    <Triangle diff={-123456} unit="원" > 
+                    <Triangle diff={worstMenu.diff} unit="원" > 
 
                     </Triangle>
                     
