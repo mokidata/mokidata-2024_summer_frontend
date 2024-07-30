3fcb891d99e43bca74166b1cad7f78a66e57f159
Apoliasm <syjsir@gmail.com>
2024-07-30 17:30:06 +0900

    FEAT : Fix logic getting menuObject

--- frontend/src/pages/ReportPages/index.jsx
+++ frontend/src/pages/ReportPages/index.jsx
@@ -13,19 +13,10 @@ import { useSelector } from "react-redux";
 function ReportPage(props){
     console.log("!!!report page!!!")
     const [page] = usePageInfo(props.page)
-    const {isLoading,todayValue,predictTodayValue,predictDetailValue,rankDetailValue,rankCompareValue,menuList} = useSalesData();
-    const [menuObject,setMenuObject] = useState({})
-    useEffect(()=> {
-        let obj = {}
-        menuList.forEach((element) => {
-            obj[element.name] = {price : element.price,img:element.img}
-        })
-        setMenuObject(obj)
-    }, [menuList]);
-
+    const {isLoadingState,todayValue,predictTodayValue,predictDetailValue,rankDetailValue,rankCompareValue,menuObject,lastDetailValue} = useSalesData();
    
     //custom hook -> 여러 데이터 받아옴
-    if(isLoading){
+    if(isLoadingState){
         console.log("isloading")
         return (
             
@@ -37,14 +28,15 @@ function ReportPage(props){
     }
     else{
         console.log("!!!Loading end!!!")
+        console.log({isLoadingState,todayValue,predictTodayValue,predictDetailValue,rankDetailValue,rankCompareValue,menuObject})
          return(
             <div className="report-page">
                 <Header page={page}></Header>
                 <TodaySales page={page} value={todayValue}></TodaySales>
                 <CompPredictSales page={page} value={predictTodayValue} todayValue={todayValue} menuObject={menuObject}></CompPredictSales>
-                <PredictSales page={page} value={predictDetailValue} menuObject={menuObject}></PredictSales>
+                <PredictSales page={page} value={predictDetailValue} menuObject={menuObject} rankCompareValue={rankCompareValue}></PredictSales>
                 <BestMenu page={page} value={rankDetailValue} menuObject={menuObject}></BestMenu>
-                <BiggestDiffMenu page={page} value={rankCompareValue} menuObject={menuObject}></BiggestDiffMenu>
+                <BiggestDiffMenu page={page} todayValue={rankDetailValue} lastDetailValue={lastDetailValue} menuObject={menuObject}></BiggestDiffMenu>
                 <BottomNavbar></BottomNavbar>
             </div>
             
