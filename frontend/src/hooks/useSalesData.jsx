e24dcce9350ec9ef6caa6113c0555c4df6bd96e0
Apoliasm <syjsir@gmail.com>
2024-07-31 11:08:08 +0900

    FIX : Fix menuObject logic

--- frontend/src/hooks/useSalesData.jsx
+++ frontend/src/hooks/useSalesData.jsx
@@ -10,7 +10,6 @@ function useSalesData() {
     const [rankCompareValue, setRankCompareValue] = useState({ daily: {}, weekly: {}, monthly: {} });
     const [rankDetailValue, setRankDetailValue] = useState({ daily: {}, weekly: {}, monthly: {} });
     const [lastDetailValue, setLastDetailValue] = useState({ daily: {}, weekly: {}, monthly: {} });
-    const [menuList, setMenuList] = useState([]);
     const [menuObject, setMenuObject] = useState({});
     const [isLoadingState, setIsLoadingState] = useState(true);
     
@@ -25,28 +24,21 @@ function useSalesData() {
             setPredictDetailValue(totalData['predictDetail']);
             setRankCompareValue(totalData['rankCompare']);
             setRankDetailValue(totalData['rankDetail']);
-            setMenuList(totalData['menuList']);
+            if (totalData['menuList'].length !== 0) {
+                const menuList = totalData['menuList']
+                let obj = {};
+                menuList.forEach((element) => {
+                    obj[element.name] = { price: element.price, img: element.img };
+                });
+                setMenuObject(obj);
+            }
             setLastDetailValue(totalData['lastDetail'])
+            setIsLoadingState(false);
         }
     }, [isLoading, totalData]); // totalData를 의존성 배열에 추가
 
-    useEffect(() => {
-        if (menuList.length !== 0) {
-            let obj = {};
-            menuList.forEach((element) => {
-                obj[element.name] = { price: element.price, img: element.img };
-            });
-            setMenuObject(obj);
-        }
-    }, [menuList]);
 
-    useEffect(() => {
-        // 모든 상태가 업데이트된 후 isLoadingState를 설정
-        if (!isLoading) {
-            setIsLoadingState(false);
-        }
-    }, [todayValue, predictTodayValue, predictDetailValue, rankDetailValue, rankCompareValue, menuObject, lastDetailValue]); // 모든 상태를 의존성 배열에 추가
-
+   
     return { isLoadingState, todayValue, predictTodayValue, predictDetailValue, rankDetailValue, rankCompareValue, menuObject , lastDetailValue };
 }
 
