c7386a3471659e460ffe408f9baf7f7cde64c4ad
Apoliasm <syjsir@gmail.com>
2024-07-31 11:05:08 +0900

    FEAT : Add SessionStorage code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,26 @@ import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ReportPage from './pages/ReportPages';
 import { Provider } from 'react-redux';
-import store from './app/store';
+import { persistor,store } from './app/store';
+import BestMenuDetail from './pages/ReportPages/BestMenuDetail';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/daily" element={<ReportPage page="daily" />} />
-          <Route path="/weekly" element={<ReportPage page="weekly" />} />
-          
-        </Routes>
+        <PersistGate loading={null} persistor={persistor}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/daily" element={<ReportPage page="daily" />} />
+            <Route path="/weekly" element={<ReportPage page="weekly" />} />
+            <Route path="/monthly" element={<ReportPage page="monthly" />} />
+            <Route path="/bestmenu" element={<BestMenuDetail page="weekly" />} />
+            
+          </Routes>
+
+        </PersistGate>
+        
       </Provider>
       
     </BrowserRouter>
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,27 @@
 import {configureStore} from '@reduxjs/toolkit'
 import { saleSlice } from '../services/salesApiSlice';
 import apiSliceReducer from './api/apiSlice';
-const store = configureStore({
+import {  PERSIST, PURGE, persistReducer, persistStore} from 'redux-persist'
+import sessionStorage from 'redux-persist/es/storage/session';
+
+
+const persistConfig = {
+    key:"root",
+    storage:sessionStorage,
+    whiteList : ["sale"]
+};
+const persistedReducer = persistReducer(persistConfig,saleSlice.reducer);
+export const store = configureStore({
     reducer:{
         login: apiSliceReducer,
-        sale : saleSlice.reducer,
+        sale : persistedReducer,
     },
+    middleware:(getDefaultMiddleWare) => 
+        getDefaultMiddleWare(
+            {
+                serializableCheck:false
+            }
+        )
 });
 
-export default store;
+export const persistor = persistStore(store);
