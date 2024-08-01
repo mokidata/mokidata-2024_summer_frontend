8de2caeb85868e228ce12b00eca1279332a82f8c
Apoliasm <syjsir@gmail.com>
2024-08-01 15:50:00 +0900

    FIX : Update chart code for highlight value

--- frontend/src/component/common/Chart.jsx
+++ frontend/src/component/common/Chart.jsx
@@ -8,27 +8,38 @@ function Chart(props){
     //     {
     //         "column": "예상 매출",
     //         "value": 99826887,
-    //         "highlight": false
+    //         "highlight": false,
+    //         "valueHighlight": false
     //     },
     //     {
     //         "column": "실제 매출",
     //         "value": 101000300,
-    //         "highlight": true
+    //         "highlight": true,
+    //         "valueHighlight" : false  
     //     }
     // ]
    
     const [value, setValue] = useState([{"column":'',"value":0}]);
     const [chartType, setChartType] = useState("bar");
     const [max,setMax] = useState(0);
+    const [min,setMin] = useState(0);
     useEffect(() => {
         setValue(props.data)
         let highestValue = -Infinity; // 초기값을 매우 작은 값으로 설정
+        let lowestValue = Infinity
         for (const element of value) {
             if (element.value > highestValue) {
                 highestValue = element.value;
             }
+            if(element.value < lowestValue){
+                if(element.value !== 0){
+                    lowestValue = element.value
+                }
+                
+            }
         }
-        setMax(highestValue)
+        setMax(highestValue);
+        setMin(lowestValue);
     });
     useEffect(() => {
         setChartType(props.type);
@@ -42,12 +53,12 @@ function Chart(props){
             {
                 value.map((data) =>(
                     <div className= "chart__data" id={data.highlight ? "highlight" : "" }>
-                        <div className="chart__value" id={data.highlight ? "highlight" : "" }>
+                        <div className="chart__value" id={data.highlight ? "highlight" : ""} style={{fontSize:data.valueHighlight? "x-large":"" , fontWeight:data.highlight ? "bold":""}}>
                             <Price value={data.value}></Price>
                         </div>
                         {
                             chartType === 'bar' ? (
-                                <BarElement max={max} value={data.value} highlight={data.highlight ? "highlight":""}></BarElement>
+                                <BarElement max={max} min={min} value={data.value} highlight={data.highlight ? "highlight":""}></BarElement>
                             ) : ""
                         }
                         <div className="chart__column" id={chartType}>
--- frontend/src/component/contents/PredictSales.jsx
+++ frontend/src/component/contents/PredictSales.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
 import Chart from "../common/Chart";
 import GetInteger from "../common/GetInteger";
-import DateConVerter from "../common/DateConverter";
+import DateConVerter, { formatMonth, formatWeek } from "../common/DateConverter";
 
 function PredictSales (props){
-    console.log(props.value)
-    console.log(props.menuObject)
     const predictValue = props.value[props.page]['predicts']
     const compareValue = props.rankCompareValue[props.page]
     const menuObject = props.menuObject
@@ -16,11 +14,10 @@ function PredictSales (props){
       compareValue[ dates].saleGetResList.forEach(element => {
         profit += element.count * element.price
       });
-      const obj = {column:DateConVerter(dates) ,value:GetInteger(profit / 10000),highlight:false}
+      const obj = {column: `${formatWeek(dates)} ` ,value:GetInteger(profit / 10000),highlight:false, valueHighlight: false}
       data.push(obj)
     }
     profit = 0
-    console.log(predictValue)
     predictValue.forEach(element=>{
       
       for (const dates of Object.keys(element.predictData)){
@@ -28,9 +25,8 @@ function PredictSales (props){
         profit += element.predictData[dates] * menuObject[element.name]['price']
       }
     })
-    const obj = {column : "예측값", value : GetInteger(profit/ 10000 ) , highlight: true }
+    const obj = {column : "예측값", value : GetInteger(profit/ 10000 ) , highlight: true , valueHighlight: false}
     data.push(obj)
-    console.log(data)
 
    
     // const data = [
@@ -38,32 +34,14 @@ function PredictSales (props){
     //       column: "3월",
     //       value: 345,
     //       highlight: false,
+    //       valueHighlight: false
     //     },
     //     {
     //       column: "4월",
     //       value: 456,
     //       highlight: false,
-    //     },
-    //     {
-    //       column: "5월",
-    //       value: 567,
-    //       highlight: false,
-    //     },
-    //     {
-    //       column: "6월",
-    //       value: 678,
-    //       highlight: false,
-    //     },
-    //     {
-    //       column: "7월",
-    //       value: 789,
-    //       highlight: false,
-    //     },
-    //     {
-    //       column: "8월",
-    //       value: 890,
-    //       highlight: true,
-    //     },
+    //       valueHighlight: false
+    //     }
     //   ];
 
     return (
