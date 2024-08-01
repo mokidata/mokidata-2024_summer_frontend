d4369942285cacd49349658935a72884076272c2
Apoliasm <syjsir@gmail.com>
2024-08-01 15:43:00 +0900

    FIX : Update navigation link in bottomNavBar

--- frontend/src/component/common/BottomNavbar.jsx
+++ frontend/src/component/common/BottomNavbar.jsx
@@ -1,10 +1,37 @@
-const BottomNavbar = () => {
+import { useNavigate } from "react-router-dom";
+
+const BottomNavbar = (props) => {
     const links = ['월별', '주별', '일별'];
+    const navigate = useNavigate()
+    function changeDateType(link){
+        let dateType = ""
+        switch(link){
+            case '월별' :
+                dateType = "monthly"
+                break
+            case '주별':
+                dateType = "weekly"
+                break
+            case '일별':
+                dateType = "daily"
+                break
+            default:
+                dateType = props.page
+        }
+        if(props.page !== dateType){
+            navigate(`/${dateType}`)
+        }
+
+        
+    }
+    
     return (
         <div className="bottom-nav">
            {
                 links.map((link)=>(
-                    <button className="botton-nav__button">
+                    <button className="botton-nav__button" onClick={(event => {
+                        changeDateType(link)
+                    })}>
                         {link}
                     </button>
                 ))
