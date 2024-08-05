b09fa66709b8fe2e058c3797ca71e62ad758f239
Apoliasm <syjsir@gmail.com>
2024-08-05 12:17:22 +0900

    DESIGN : Modify button shape following props.shape

--- frontend/src/component/common/Button.jsx
+++ frontend/src/component/common/Button.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function Button (props){    
     return (
-        <button className="button" id={props.color}>
+        <button className="button" 
+            id={props.color} 
+            style={{borderRadius: 
+                props.shape === 'rect' ? '10px' :
+                props.shage === 'circle' ?'25px':
+                '25px'                                        
+        }}>
             {props.txt}
         </button>
     )
