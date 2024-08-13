8cfe91e0929a8f3984fd990a533b287b6bea1e01
Apoliasm <syjsir@gmail.com>
2024-08-13 15:32:33 +0900

    CHORE : Fix some info in admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import Login from './pages/LoginPages/index'
-import TodaySales from './component/contents/TodaySales';
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ReportPage from './pages/ReportPages';
 import { Provider } from 'react-redux';
 import { persistor,store } from './app/store';
-import BestMenuDetail from './pages/ReportPages/BestMenuDetail';
 import { PersistGate } from 'redux-persist/integration/react';
-import BiggestDiffDetail from './pages/ReportPages/BiggestDiffDetail';
 import AdminLogin from './pages/LoginPages/AdminLogin';
 import AdminIndex from './pages/AdminPages/AdminIndex';
 import { useEffect } from 'react';
+import DetailPage from './pages/ReportPages/DetailPage';
 
 function App() {
   function setScreenSize() {
@@ -30,8 +28,7 @@ function App() {
             <Route path="/daily" element={<ReportPage page="daily" />} />
             <Route path="/weekly" element={<ReportPage page="weekly" />} />
             <Route path="/monthly" element={<ReportPage page="monthly" />} />
-            <Route path="/bestmenu" element={<BestMenuDetail page="daily" />} />
-            <Route path="/biggestdiff" element={<BiggestDiffDetail page="daily" />}/>
+            <Route path='/detail' element={<DetailPage page="daily"/>}/>
             <Route path="/admin" element={<AdminLogin /> }></Route>
             <Route path="/admin/main" element={<AdminIndex/>}></Route>
           </Routes>
--- frontend/src/component/admin/InputName.jsx
+++ frontend/src/component/admin/InputName.jsx
@@ -6,7 +6,7 @@ function InputName(props){
     return(
         <div className="input-name">
             <div className="input-name__name">
-                {props.userName}
+                {props.userName === "" ? "매장명 입력" : props.userName}
 
             </div>
             <button className="input-name__button" onClick={props.openModal}>
