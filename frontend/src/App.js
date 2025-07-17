4ea5b924eee740a616aaf49530eab8fd86c15d2d
heydylee <sub09065@gmail.com>
2025-07-17 11:49:36 +0900

    Fix : 카카오톡 인 앱 브라우저 사용 시 innerheight 측정 오류 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import Login from './pages/ReportPages/ReportLogin'
-import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import ReportPage from './pages/ReportPages/ReportPage';
-import { Provider } from 'react-redux';
-import { persistor,store } from './store/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import AdminLogin from './pages/AdminPages/AdminLogin';
-import AdminIndex from './pages/AdminPages/AdminIndex';
-import DetailPage from './pages/ReportPages/DetailPage';
-import Home from './pages/Home';
-import PointPage from './pages/PointPages/PointPage';
-import PointDetails from './pages/PointPages/PointDetails';
-import PONO from './pages/PointPages/PONO';
-import { useEffect, useState } from 'react';
+import Login from "./pages/ReportPages/ReportLogin";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ReportPage from "./pages/ReportPages/ReportPage";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import AdminLogin from "./pages/AdminPages/AdminLogin";
+import AdminIndex from "./pages/AdminPages/AdminIndex";
+import DetailPage from "./pages/ReportPages/DetailPage";
+import Home from "./pages/Home";
+import PointPage from "./pages/PointPages/PointPage";
+import PointDetails from "./pages/PointPages/PointDetails";
+import PONO from "./pages/PointPages/PONO";
+import { useEffect, useState } from "react";
 
 function App() {
   const [alertShown, setAlertShown] = useState(false);
 
   useEffect(() => {
     const handleOrientation = () => {
-      const isLandscape = window.innerWidth > window.innerHeight;
+      const isLandscape = window.matchMedia("(orientation: landscape)").matches;
       if (isLandscape && !alertShown) {
         alert("가로 모드는 지원되지 않습니다. 세로 모드로 전환해주세요!");
         setAlertShown(true);
@@ -39,22 +39,22 @@ function App() {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/daily" element={<ReportPage page="daily" />} />
             <Route path="/weekly" element={<ReportPage page="weekly" />} />
             <Route path="/monthly" element={<ReportPage page="monthly" />} />
-            <Route path='/detail' element={<DetailPage page="daily"/>}/>
-            <Route path="/admin" element={<AdminLogin /> }></Route>
-            <Route path="/admin/main" element={<AdminIndex/>}></Route>
-            <Route path='/point/store_id/:store_id/phone_num/:phone_num' element={<PointPage />} />
-            <Route path='/PONO' element={ <PONO /> } />
+            <Route path="/detail" element={<DetailPage page="daily" />} />
+            <Route path="/admin" element={<AdminLogin />}></Route>
+            <Route path="/admin/main" element={<AdminIndex />}></Route>
+            <Route
+              path="/point/store_id/:store_id/phone_num/:phone_num"
+              element={<PointPage />}
+            />
+            <Route path="/PONO" element={<PONO />} />
           </Routes>
-
         </PersistGate>
-        
       </Provider>
-      
     </BrowserRouter>
   );
 }
